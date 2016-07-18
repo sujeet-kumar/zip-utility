77ea2e1e9e012524ae4e743a615c4c1c5260775a
Sujeet Kumar <sujeetkv90@gmail.com>
2016-07-18 10:59:37 +0530

    added composer support

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 Zip utility class
 
 -by Sujeet <sujeetkv90@gmail.com>
+
+Install using composer:
+
+    {
+        "repositories": [
+            {
+                "type": "vcs",
+                "url": "https://github.com/sujeet-kumar/zip-utility"
+            }
+        ],
+        "require": {
+            "sujeet-kumar/zip-utility": "dev-master"
+        }
+    }
+
--- example.php
+++ example.php
@@ -1,15 +1,15 @@
 <?php
-require_once 'Zip.php';
+require_once './src/Zip.php';
 
-Zip::compress('./dir', './dirc1.zip');
+Zip::compress('./dir', './zipfile.zip');
 
-Zip::compress(array('./example.php','./zip.php','./dirc1.zip'), './newfilename.zip');
+Zip::compress(array('./example.php','./test.php','./zipfile.zip'), './newfilename.zip');
 
 Zip::addComment('./newfilename.zip', 'demo comment');
 
-Zip::extract('./dirc1.zip');
+Zip::extract('./zipfile.zip');
 
-print_r(Zip::getList('./dirc1.zip'));
+print_r(Zip::getList('./zipfile.zip'));
 
-print_r(Zip::getTree('./dirc1.zip'));
+print_r(Zip::getTree('./zipfile.zip'));
 
